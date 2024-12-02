0703ebf374aef8056f2dc98993fecb0241010f92
Rafaela Silverio <rafaelapetelin@gmail.com>
2024-12-02 19:24:16 -0300

    Alternar nome

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ function App() {
   const [nome, setNome] = useState('Fórum Ops');
 
   function mudarNome() {
-    setNome('Suporte Educacional');
+    if(nome === 'Fórum Ops')
+      setNome('Suporte Educacional');
+    else{
+      setNome('Fórum Ops');
+    }
   }
 
   return (
     <>
       <div>
-        <img src={logo} className="logo react" alt="React logo" />
+        <img src={logo} className="logo" alt="Logo de Suporte Educacional" />
       </div>
 
       <h1>{nome}</h1>
@@ -22,6 +26,7 @@ function App() {
       <button onClick={mudarNome}>
         Clique para mudar o nome
       </button>
+ 
     </>
   )
 }
